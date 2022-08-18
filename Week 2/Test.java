d483ac4fcbb18257ca7262cdd745c4bfb13517af
Binit <binitkoirala17@gmail.com>
2022-08-18 08:23:51 +0545

    added difference and multiplication class

--- Week 2/Test.java
+++ Week 2/Test.java
@@ -12,10 +12,22 @@ public class Test {
 
         System.out.print("Enter your second number: ");
         double secondNumber = scanner.nextDouble();
-        
+
+        System.out.print("Press + to add, - to subtract and * to multiply: ");
+        scanner.nextLine();
+        String option = scanner.nextLine();
+
         Output myOutput = new Output();
-        Output.getMyName();
-        System.out.println(myOutput.getDetails(firstNumber, secondNumber));
+        String response;
+        if (option.contains("+"))  {
+            response = myOutput.getSumDetails(firstNumber, secondNumber);
+        } else if(option.contains("-")) {
+            response = myOutput.getDifferenceDetails(firstNumber, secondNumber);
+        } else {
+            response = myOutput.getMultiplyDetails(firstNumber, secondNumber);
+        }
+        
+        System.out.println(response);
         scanner.close();
     }
 }
