5c7a5379d77046ad1ffcd31e2334dca552d842c9
Binit <binitkoirala17@gmail.com>
2022-11-15 08:49:38 +0545

    added string replace functionality in method

---
+++ Week6/StringClass.java
@@ -0,0 +1,34 @@
+package Week6;
+
+public class StringClass {
+    public static void main(String[] args) {
+        try {
+
+            // charAt() -> gets char in the given index in a string
+            // indexOf() -> gets index of a char
+            // toLowerCase()
+            // toUpperCase()
+            // length()
+            // contains()
+            // substring()
+            // trim()
+            // replace()
+            // compareTo()
+            // equalsIgnoreCase()
+
+            String name = " Suneeel   Thapa ";
+            String[] list = name.trim().split(" ");
+            String goodName = "";
+            for (String ss : list) {
+                if (!ss.isEmpty()) {
+                    goodName = goodName + " " + ss;
+                }
+            }
+            System.out.println(goodName.trim());
+
+        } catch (Exception ex) {
+            System.out.println("Error detected");
+        }
+
+    }
+}
